e45e4704e176585a6bc2fa63e395dc060c043d14
semyon <semyo@DESKTOP-PPV20KR>
2021-03-02 10:13:03 +0300

    Добавьте файлы проекта.

---
+++ Emulator/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Emulator
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			SmartCamera.Closing();
+			SafetySystem.Closing();
+			RemoteTerminal.Closing();
+			RobotSimulator.Closing();
+		}
+	}
+}
